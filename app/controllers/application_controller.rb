995a753bb64e6f91c4650ef4a40107dd24b77590
pndpo <peterdepaulo@gmail.com>
2014-10-24 17:26:33 -0700

    digest the usernames

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with  :name => "foo",
-                                :password => "bar",
-                                :only => [:create, :update, :edit, :new, :destroy]
+  USERS = {"foo" => "bar"}
+  before_action :authenticate_user
+
+  private
+
+  def authenticate_user
+  	authenticate_or_request_with_http_digest do |username|
+      USERS[username]
+    end
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  skip_filter :authenticate_user, only: [:show, :index]
   # GET /users
   # GET /users.json
   def index
