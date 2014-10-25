cf5680fff86cdd5ac34c0112ece2fb5daebee802
pndpo <peterdepaulo@gmail.com>
2014-10-24 17:20:50 -0700

    users can only see index and show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  http_basic_authenticate_with  :name => "foo", 
-                                  :password => "bar",
-                                  :only => [:show, :index]
+  http_basic_authenticate_with  :name => "foo",
+                                :password => "bar",
+                                :only => [:create, :update, :edit, :new, :destroy]
 
 end
-
